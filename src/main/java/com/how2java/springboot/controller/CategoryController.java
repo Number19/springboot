870d6f6b74f2f21ee7c19e2e49f00bb59405340b
石有傲 <844261278@qq.com>
2018-08-09 15:10:26 +0800

    66666

--- src/main/java/com/how2java/springboot/controller/CategoryController.java
+++ src/main/java/com/how2java/springboot/controller/CategoryController.java
@@ -10,11 +10,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 
 /**
  * @Author sya
